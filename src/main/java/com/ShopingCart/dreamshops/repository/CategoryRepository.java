3f5541150d14fafd58cc57ba106fec674596dcd0
Tobias Runge <Tobias.Runge95@gmx.de>
2025-07-16 08:50:43 +0200

    changed existsByName in CategoryRepository

--- src/main/java/com/ShopingCart/dreamshops/repository/CategoryRepository.java
+++ src/main/java/com/ShopingCart/dreamshops/repository/CategoryRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByName(String name);
+    Boolean existsByName(String name);
 }
