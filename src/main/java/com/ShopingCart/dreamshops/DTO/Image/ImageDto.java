5a9ce281b033be95cb7c041806e960f2c6aadd32
Tobias Runge <Tobias.Runge95@gmx.de>
2025-07-16 08:51:08 +0200

    Added ImageDto

---
+++ src/main/java/com/ShopingCart/dreamshops/DTO/Image/ImageDto.java
@@ -0,0 +1,11 @@
+package com.ShopingCart.dreamshops.DTO.Image;
+
+import lombok.Data;
+
+@Data
+public class ImageDto {
+    private Long imageId;
+    private String imageName;
+    private String downloadUrl;
+
+}
